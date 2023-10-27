2ab2a246e21d7897cdb1946d25129ae2a33b84ae
AzerothA <telifesite@gmail.com>
2023-10-27 21:07:16 +0330

    filter file added

--- src/filter.rs
+++ src/filter.rs
@@ -1,13 +1,24 @@
-pub struct Sort {}
-pub struct Order {}
-pub struct From {}
-pub struct Limit {}
+/// Sort (SortBy)
+pub enum Sort {
+    /// Sort by number
+    Number,
+
+    /// Sort by Name
+    Name,
+}
+
+/// Order the result list by ASC or DESC
+pub enum Order {
+    // ASC
+    Asc,
+
+    // DESC
+    Desc,
+}
 
 /// Possible Filters for Database Table
 /// This list may change in future updates
 pub struct Filter {
-    sort: Sort,
-    order: Order,
-    from: From,
-    limit: Limit,
+    pub sort: Sort,
+    pub order: Order,
 }
