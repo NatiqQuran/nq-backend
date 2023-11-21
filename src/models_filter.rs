8f1686f67caf0271b169ff9a53e8a440b0118152
AzerothA <telifesite@gmail.com>
2023-11-21 17:59:42 +0330

    Filter impl to Translation Model

--- src/models_filter.rs
+++ src/models_filter.rs
@@ -1,10 +1,10 @@
-use crate::models::QuranMushaf;
+use crate::models::{QuranMushaf, QuranSurah, Translation};
 use crate::schema::mushafs::{table as quran_mushafs_table, BoxedQuery as MushafBoxedQuery};
 use crate::schema::quran_surahs::{table as quran_surahs_table, BoxedQuery as SurahBoxedQuery};
+use crate::schema::translations::{table as translations_table, BoxedQuery as TranslationBoxedQuery};
 use crate::{
     error::RouterError,
     filter::{Filter, Filters, Order},
-    models::QuranSurah,
 };
 use diesel::pg::Pg;
 use diesel::{prelude::*, query_dsl::methods::BoxedDsl};
@@ -45,7 +45,11 @@ impl Filter for QuranSurah {
                     Order::Asc => quran_surahs.order(number.asc()).internal_into_boxed(),
                     Order::Desc => quran_surahs.order(number.desc()).internal_into_boxed(),
                 }),
-                _ => Err(RouterError::BadRequest("No such sort value!".to_string())),
+
+                value => Err(RouterError::BadRequest(format!(
+                    "Sort value {} is not possible!",
+                    value
+                ))),
             },
 
             None => Ok(quran_surahs.internal_into_boxed()),
@@ -77,10 +81,48 @@ impl Filter for QuranMushaf {
                     Order::Desc => mushafs.order(name.desc()).internal_into_boxed(),
                 }),
 
-                _ => Err(RouterError::BadRequest("No such sort value!".to_string())),
+                value => Err(RouterError::BadRequest(format!(
+                    "Sort value {} is not possible!",
+                    value
+                ))),
             },
 
             None => Ok(mushafs.internal_into_boxed()),
+        }?;
+
+        _query = match filters.to() {
+            Some(limit) => _query
+                .limit(limit as i64)
+                .offset(filters.from().unwrap_or_default() as i64),
+            None => _query.offset(filters.from().unwrap_or_default() as i64),
+        };
+
+        Ok(_query)
+    }
+}
+
+impl Filter for Translation {
+    type Output = Result<TranslationBoxedQuery<'static, Pg>, RouterError>;
+
+    fn filter(filters: Box<dyn Filters>) -> Self::Output {
+        use crate::schema::translations::dsl::*;
+
+        let mut _query = translations_table.into_boxed();
+
+        _query = match filters.sort() {
+            Some(sort_str) => match sort_str.as_str() {
+                "createTime" => Ok(match filters.order().unwrap_or_default() {
+                    Order::Asc => translations.order(created_at.asc()).internal_into_boxed(),
+                    Order::Desc => translations.order(created_at.desc()).internal_into_boxed(),
+                }),
+
+                value => Err(RouterError::BadRequest(format!(
+                    "Sort value {} is not possible!",
+                    value
+                ))),
+            },
+
+            None => Ok(translations.internal_into_boxed()),
         }?;
 
         _query = match filters.to() {
