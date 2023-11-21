7e284a6e401508b95fc7536c8479e7987cc28d9e
AzerothA <telifesite@gmail.com>
2023-11-21 17:59:59 +0330

    translation and mushaf filters works.

--- src/routers/translation/translation_list.rs
+++ src/routers/translation/translation_list.rs
@@ -1,20 +1,23 @@
 use crate::error::RouterError;
+use crate::filter::Filter;
 use crate::models::Translation;
 use crate::DbPool;
 use actix_web::web;
 use diesel::prelude::*;
 
+use super::TranslationListQuery;
+
 /// Returns the list of translations
 pub async fn translation_list(
     pool: web::Data<DbPool>,
+    web::Query(query): web::Query<TranslationListQuery>,
 ) -> Result<web::Json<Vec<Translation>>, RouterError> {
-    use crate::schema::translations::dsl::*;
-
     let result = web::block(move || {
         let mut conn = pool.get().unwrap();
 
         // Get the list of translations from the database
-        let translations_list = translations.load::<Translation>(&mut conn)?;
+        let translations_list =
+            Translation::filter(Box::from(query))?.load::<Translation>(&mut conn)?;
 
         Ok(web::Json(translations_list))
     })
