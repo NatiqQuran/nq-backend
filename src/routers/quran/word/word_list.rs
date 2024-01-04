03b955f24fd356505ba12be321c3a5a9771db9cb
AzerothA <telifesite@gmail.com>
2024-01-05 02:59:25 +0330

    Filters added

--- src/routers/quran/word/word_list.rs
+++ src/routers/quran/word/word_list.rs
@@ -1,18 +1,24 @@
 use crate::error::RouterError;
+use crate::filter::Filter;
 use crate::models::QuranWord;
 use crate::DbPool;
 use actix_web::web;
 use diesel::prelude::*;
 
-/// Returns the list of quran_words
-pub async fn word_list(pool: web::Data<DbPool>) -> Result<web::Json<Vec<QuranWord>>, RouterError> {
-    use crate::schema::quran_words::dsl::*;
+use super::WordListQuery;
 
+/// Returns the list of quran_words
+pub async fn word_list(
+    pool: web::Data<DbPool>,
+    web::Query(query): web::Query<WordListQuery>,
+) -> Result<web::Json<Vec<QuranWord>>, RouterError> {
     web::block(move || {
         let mut conn = pool.get().unwrap();
 
+        let filtered_words = QuranWord::filter(Box::from(query))?;
+
         // Get the list of words from the database
-        let words_list = quran_words.load::<QuranWord>(&mut conn)?;
+        let words_list = filtered_words.load::<QuranWord>(&mut conn)?;
 
         Ok(web::Json(words_list))
     })
--- src/routers/translation/translation_list.rs
+++ src/routers/translation/translation_list.rs
@@ -12,12 +12,32 @@ pub async fn translation_list(
     pool: web::Data<DbPool>,
     web::Query(query): web::Query<TranslationListQuery>,
 ) -> Result<web::Json<Vec<Translation>>, RouterError> {
+    //use crate::schema::app_accounts::dsl::{app_accounts, id as acc_id, uuid as account_uuid};
+    use crate::schema::translations::dsl::language;
+
     let result = web::block(move || {
         let mut conn = pool.get().unwrap();
 
+        // Get the given language or return the default
+        let lang = match query.language {
+            Some(ref s) => s.clone(),
+            None => "en".to_string(),
+        };
+
+        // TODO: FIX
+        //let master_account: Vec<i32> = match query.master_account {
+        //    Some(uuid) => vec![app_accounts
+        //        .filter(account_uuid.eq(uuid))
+        //        .select(acc_id)
+        //        .get_result(&mut conn)?],
+        //    None => vec![],
+        //};
+
         // Get the list of translations from the database
-        let translations_list =
-            Translation::filter(Box::from(query))?.load::<Translation>(&mut conn)?;
+        let translations_list = Translation::filter(Box::from(query))?
+            .filter(language.eq(lang))
+            //.filter(translator_account_id.eq_any::<Vec<i32>>(master_account))
+            .load::<Translation>(&mut conn)?;
 
         Ok(web::Json(translations_list))
     })
