b930a9e61c7243ffe58b2a8fc1902fa3b90416d8
AzerothA <telifesite@gmail.com>
2024-06-11 13:16:45 +0330

    Use new authZ and authN updates

--- src/token_checker.rs
+++ src/token_checker.rs
@@ -1,5 +1,7 @@
-use crate::{models::Token, DbPool};
-use actix_web::web;
+use std::sync::Arc;
+
+use crate::{error::RouterError, models::Token, DbPool};
+use actix_web::{web, ResponseError};
 use async_trait::async_trait;
 use auth_n::token::{HashBuilder, TokenChecker};
 use diesel::prelude::*;
@@ -19,11 +21,13 @@ impl UserIdFromToken {
 
 #[async_trait]
 impl TokenChecker<u32> for UserIdFromToken {
-    async fn get_user_id(&self, request_token: &str) -> Option<u32> {
+    async fn get_user_id(&self, request_token: &str) -> Result<u32, Box<dyn ResponseError>> {
         use crate::schema::app_tokens::dsl::*;
 
         // Token as bytes
         let token_bytes: Vec<u8> = request_token.bytes().collect();
+
+        let token_invalid_error = Box::new(RouterError::from_predefined("AUTHN_TOKEN_INVALID"));
 
         let mut conn = self.db_pool.get().unwrap();
 
@@ -46,16 +50,25 @@ impl TokenChecker<u32> for UserIdFromToken {
 
         // Is there any token we found ?
         if token.is_empty() {
-            return None;
+            token_invalid_error.log_to_db(Arc::new(self.db_pool.clone()));
+            return Err(token_invalid_error);
         }
 
         let last_token = token.get(0).unwrap();
 
         // Return None for teminated token
         if last_token.terminated {
-            return None;
+            token_invalid_error.log_to_db(Arc::new(self.db_pool.clone()));
+            return Err(token_invalid_error);
         }
 
-        Some(last_token.user_id as u32)
+        Ok(last_token.user_id as u32)
+    }
+
+    async fn token_not_found_error(&self) -> Box<dyn ResponseError> {
+        Box::new(
+            RouterError::from_predefined("AUTHN_TOKEN_NOT_FOUND")
+                .log_to_db(Arc::new(self.db_pool.clone())),
+        )
     }
 }
