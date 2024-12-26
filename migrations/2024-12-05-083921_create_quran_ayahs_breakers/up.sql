4ae05a4e5184ac6a277c8cf45431408aa4f88e28
nyzd <azerothadev@gmail.com>
2024-12-26 18:41:56 +0330

    tables updated

--- migrations/2024-12-05-083921_create_quran_ayahs_breakers/up.sql
+++ migrations/2024-12-05-083921_create_quran_ayahs_breakers/up.sql
@@ -5,6 +5,8 @@ CREATE TABLE quran_ayahs_breakers (
     ayah_id serial NOT NULL,
     owner_account_id INT REFERENCES app_accounts (id),
     name VARCHAR(256) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT ayah_break_id PRIMARY KEY (id),
     CONSTRAINT fk_quran_ayah_break_creator_user_id FOREIGN KEY (creator_user_id) REFERENCES app_users (id),
     CONSTRAINT fk_break_ayah FOREIGN KEY (ayah_id) REFERENCES quran_ayahs (id) on delete cascade
